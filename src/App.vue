9b7757784c26c8e5186cf1f13d8b860b1a8e25f0
Classican67 <jeremy.maitre.1@ulaval.ca>
2023-12-17 16:07:17 -0500

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field
+          v-model="email"
+          label="First name"
+      ></v-text-field>
+
+      <v-text-field
+          v-model="password"
+          label="Last name"
+      ></v-text-field>
+
+      <v-btn @click="onRegister()">Register</v-btn>
+      <v-btn @click="onLogin()">Login</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+
+
+<script setup>
+
+import {ref} from "vue";
+import {login, register} from "./api/register.js"
+
+const email =ref(null)
+const password =ref(null)
+
+async function onRegister() {
+await register(email.value, password.value)
+}
+
+async function onLogin() {
+  await login(email.value, password.value)
+}
+
+</script>
+
+
+
+
+<style></style>
---
+++ src/api/register.js
@@ -0,0 +1,47 @@
+const url = "http://localhost:8000"
+import Cookies from "js-cookie"
+let token = null;
+
+
+export const register = async (email,password) => {
+    try {
+        const response = await fetch(`${url}/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+        if (response.ok) {
+            console.log("Success to register")
+        }
+    }
+    catch(err){
+        console.log("Unable to register :"+err)
+    }
+}
+
+export const login = async (email, password) => {
+    try {
+        const response = await fetch(`${url}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+        if (response.ok) {
+            const data = await response.json();
+            Cookies.set("user_token", data.token);
+        }
+    }
+    catch(err){
+        console.log(err)
+    }
+}
